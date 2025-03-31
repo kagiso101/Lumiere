7b83b117efa150587ec0a720bd2b8c8d9004ffcf
kagisoh@rain.co.za <kagisoh@rain.co.za>
2025-03-31 18:48:01 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const countdownDate = new Date("2025-06-01T00:00:00").getTime();
+
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        document.querySelector(".countdown").innerHTML = "<p>We are live now!</p>";
+    }
+};
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
